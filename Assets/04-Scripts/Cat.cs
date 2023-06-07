0838002934b930f1b0b754b7dc87ce2bb9fa3686
SCU Imaginarium (Link) <hasmikgalstyan33@gmail.com>
2023-06-06 18:07:14 -0700

    Prototype Complete
    Character animations + glow effect + teleporting between rooms

--- Assets/04-Scripts/Cat.cs
+++ Assets/04-Scripts/Cat.cs
@@ -5,6 +5,19 @@ public class Cat : MonoBehaviour
 {
     void OnTriggerEnter(Collider other)
     {
-        if (other.tag == "Door") SceneManager.LoadScene("Bedroom");
+        if (other.gameObject.tag == "Door")
+        {
+            if (SceneManager.GetActiveScene().name == "Kitchen") SceneManager.LoadScene("Bedroom");
+            else SceneManager.LoadScene("Kitchen");
+
+            changePosition(SceneManager.GetActiveScene().name);
+        }
+    }
+
+    void changePosition(string name) // not working in kitchen
+    {
+        GameObject cat = GameObject.Find("Player");
+        if (name == "Bedroom") cat.transform.position = new Vector3(-26.75f, 0, 19.74f);
+        else cat.transform.position = new Vector3(9.92f, 0, 19.97f);
     }
 }
--- Assets/04-Scripts/Girl.cs
+++ Assets/04-Scripts/Girl.cs
@@ -2,18 +2,37 @@ using UnityEngine;
 
 public class Girl : MonoBehaviour
 {
+    [SerializeField] private Light spotlight;
+
+    private GameObject target;
     private Animator anim;
+    private float speed = 10.0f;
+    private Vector3 targetDirection;
 
     void Start()
     {
         anim = GetComponent<Animator>();
+        target = GameObject.FindGameObjectWithTag("Door");
     }
 
     void OnTriggerEnter(Collider other)
     {
-        if (other.tag == "Important")
+        if (other.gameObject.tag == "Important") anim.SetTrigger("Pet");
+        if (other.gameObject.tag == "Door") Destroy(gameObject);
+    }
+
+    private void Update()
+    {
+        if (anim.GetCurrentAnimatorStateInfo(0).IsName("Walk"))
         {
-            anim.SetTrigger("Pet");
+            var step = speed * Time.deltaTime;
+            transform.position = Vector3.MoveTowards(transform.position, target.transform.position, step);
+            targetDirection = target.transform.position - transform.position;
+            Vector3 newDirection = Vector3.RotateTowards(transform.forward, targetDirection, step, 0.0f);
+            transform.rotation = Quaternion.LookRotation(newDirection);
+
+            Vector3 temp = new Vector3(transform.position.x, spotlight.transform.position.y, transform.position.z);
+            spotlight.transform.position = temp;
         }
     }
 }
